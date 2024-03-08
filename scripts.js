82e0a830c6ed15f0adb182628043fcf5a5f17f66
Antonio Marmolejo <djcamg71@gmail.com>
2024-03-08 10:37:06 -0500

    feat: creación de una vueva tarjeta para el día de la madre

--- scripts.js
+++ scripts.js
@@ -4,11 +4,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const text2 = document.querySelector(".cards2");
     const text3 = document.querySelector(".cards3");
     const openCardsLeft = document.querySelector(".front");
-    valentineCard.addEventListener("click", function () {
+
+    const dayMother = document.querySelector(".mother");
+    const textTwo = document.querySelector(".cards1-2");
+    const text2Two = document.querySelector(".cards2-2");
+    const text3Two = document.querySelector(".cards3-2");
+    const text4Two = document.querySelector(".cards3-2_edit");
+    const openCardsLeftTwo = document.querySelector(".front2");
+
+    function openCard() {
         valentineCard.classList.toggle("closed");
         text.classList.toggle("open");
         text2.classList.toggle("open1");
         text3.classList.toggle("open3");
         openCardsLeft.classList.toggle("frontOpen2");
-    });
+    }
+    function openCard2() {
+        dayMother.classList.toggle("closed");
+        textTwo.classList.toggle("open");
+        text2Two.classList.toggle("open1");
+        text3Two.classList.toggle("open3");
+        text4Two.classList.toggle("open4");
+        openCardsLeftTwo.classList.toggle("frontOpen2");
+    }
+
+    valentineCard.addEventListener("click", openCard);
+    dayMother.addEventListener("click", openCard2);
 });
